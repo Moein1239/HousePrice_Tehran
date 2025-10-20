f0c9ef8fca6eb5c7d7947c21c37bf14d2187e7ac
Moein Azizian <azizian1239@gmail.com>
2025-10-20 19:13:30 +0330

    Complete Django Api and also Added HTML

--- houseprice_Predictor/predictor/serializers.py
+++ houseprice_Predictor/predictor/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 
 class HouseDataSerializer(serializers.Serializer):
-    Priceـperـsquareـmeter = serializers.FloatField()
+    Priceـperـsquareـmeter  = serializers.FloatField()
     Area = serializers.FloatField()
     Warehouse=serializers.FloatField()
     Elevator=serializers.FloatField()
     Parking = serializers.FloatField()
     Room = serializers.FloatField()
     Address= serializers.CharField()
+    
